5fc03023c62201d2af885881d8a231f81fcdaaf7
Israel Calderon <icalderond@outlook.com>
2015-06-02 11:40:54 -0500

    Conexion a la base de datos

---
+++ controlador/database.php
@@ -0,0 +1,29 @@
+<?php
+// Conectando, seleccionando la base de datos
+$link = mysql_connect('localhost', 'root', '')
+    or die('No se pudo conectar: ' . mysql_error());
+echo 'Connected successfully';
+mysql_select_db('cdcol') or die('No se pudo seleccionar la base de datos');
+
+$query = 'SELECT * FROM cds';
+$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+
+
+
+// Imprimir los resultados en HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Liberar resultados
+mysql_free_result($result);
+
+// Cerrar la conexión
+mysql_close($link);
+?>
